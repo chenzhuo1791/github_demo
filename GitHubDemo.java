036b1f7c8537be30dbbe754050d929b484eb1441
chenzhuo1791 <37822707+chenzhuo1791@users.noreply.github.com>
2018-04-04 16:51:23 +0800

    GitHubDemo
    number github demo java file

---
+++ GitHubDemo.java
@@ -0,0 +1,12 @@
+
+public class GitHubDemo {
+
+	public static void main(String[] args) {
+		int result = 1 ;
+		for (int i = 1 ; i < 10 ; i++) {
+			result = result * i ;
+		}
+		System.out.println(" @@@  result : " + result);
+	}
+
+}
