90e9bf51c308f022c725b9ffb6ac87bd362c0e4b
chenzhuo <15161171791@163.com>
2018-04-04 17:29:19 +0800

    更新其中一个文件

--- GitHubDemo.java
+++ GitHubDemo.java
@@ -1,12 +1,11 @@
 
 public class GitHubDemo {
-
 	public static void main(String[] args) {
 		int result = 1 ;
-		for (int i = 1 ; i < 10 ; i++) {
+		for (int i = 1 ; i < 7 ; i++) {
 			result = result * i ;
 		}
-		System.out.println(" @@@  result : " + result);
+		System.out.println(" ### result : " + result);
 	}
 
 }
