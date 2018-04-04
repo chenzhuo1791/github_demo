363b11b2e60b3828029de5cf6c85c9aaf65121e2
chenzhuo1791 <37822707+chenzhuo1791@users.noreply.github.com>
2018-04-04 17:37:56 +0800

    add a line to the code
    add a line to the code  message !

--- GitHubDemo.java
+++ GitHubDemo.java
@@ -5,6 +5,7 @@ public class GitHubDemo {
 		for (int i = 1 ; i < 7 ; i++) {
 			result = result * i ;
 		}
+		System.out.println(" !!! hellworld @@  " );
 		System.out.println(" ### result : " + result);
 	}
 
